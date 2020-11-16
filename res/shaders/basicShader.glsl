cf4e0e1174d6e79f3a135858bc7a0a76fcc16d7c
Ron Malka <Ron.Malka@motorolasolutions.com>
2020-11-16 18:05:39 +0200

    before lec

--- res/shaders/basicShader.glsl
+++ res/shaders/basicShader.glsl
@@ -11,9 +11,66 @@ uniform ivec4 sizes; //{number of objects , number of lights , width, hight}
 
 in vec3 position1;
 
+struct Hit 
+{
+    float enter;
+    float exit;
+};
+
 // vector dir - calc eye and pixel vector
 
+vec3 calcDirVec() 
+{
+    int width = sizes.z;
+    int high = sizes.w;
+
+    vec2 pc = vec2(high / 2 , width / 2 ); 
+
+    vec3 p0 = vec3 (eye.xyz );
+
+    vec3 v= vec3(0,1,0) ;
+ 
+    vec3 t = normalize(position1 - eye);
+
+    vec3 b = normalize(cross(v,t));
+
+    vec3 v_normal = cross(t,b);
+    
+    vec3 c = eye.xyz + t;
+
+    float rx = width /2 ;
+    float ry = high / 2;
+    float R = 1 ;
+
+    vec3 p = pc + b*(position1.x - abs(rx/2)) * R - v_normal * (position1.y - abs(ry/2)) * R ;
+
+     
+}
+
 //calc shape intersect - sphere, plane
+
+bool intersectionSphere(vec3 sourcePoint,vec3 v, vec4 sphereInfo, out Hit hit)
+{
+    float r =sphereInfo.w;
+    float t = dot(sourcePoint - sphereInfo.xyz , v);
+    vec3 p = sourcePoint + t * v;
+    float y = length(sourcePoint - p);
+    float x = r*r - y*y;
+    
+    if(x<0)
+        return false;
+
+    x= sqrt(x);
+    float t1 = x + t;
+    float t2 = x - t;
+
+    hit.enter = t1;
+    hit.exit = t2;
+
+    return true;
+    
+}
+
 
 //loop for every object and check hit with the ray
 
@@ -24,8 +81,44 @@ in vec3 position1;
 
 float intersection(vec3 sourcePoint,vec3 v)
 {
-    
-    return 0;
+    float color;
+    Hit hit;
+    int i = 0 ; 
+
+    int minHitIndex = -1;
+    float minDistance = 999999.;
+    vec3 enter = vec3(0,0,0);
+    for( i=0; i < sizes.x; i++)
+    {
+        if (objects[i].w >= 0){
+            if(intersectionSphere(sourcePoint, v, objects[i],hit)){ 
+                if(hit.enter < minDistance){
+                    enter = sourcePoint.xyz + v*hit.enter; 
+                    minHitIndex = i;
+                    minDistance = hit.enter;
+                }  
+                color = calcColor(); 
+           }
+        }     
+        else{
+            if(intersectionPlane(sourcePoint, v, objects[i],hit)){
+                if(hit.enter < minDistance){
+                    enter = sourcePoint.xyz + v*hit.enter;
+                    minHitIndex = i;
+                    minDistance = distance;
+                }  
+                color = calcColor();   
+            }
+        }
+         
+    }
+    if(minHitIndex != -1){
+        color += calcLight(objects[minHitIndex]);
+    }
+    else{
+        color = backgroundColor;
+    }
+    return color;
     
 }
 
@@ -38,6 +131,5 @@ vec3 colorCalc( vec3 intersectionPoint)
 
 void main()
 {  
-   
-            gl_FragColor = vec4(colorCalc(eye.xyz),1);      
+   gl_FragColor = vec4(colorCalc(eye.xyz),1);      
 }
